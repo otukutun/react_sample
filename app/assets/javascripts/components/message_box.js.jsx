66b6f254c7b19e40ad9f927c45d2e1a7d2c37efe
otukutun <orehaorz@gmail.com>
2016-12-25 22:59:28 +0900

    ADD.

--- app/assets/javascripts/components/message_box.js.jsx
+++ app/assets/javascripts/components/message_box.js.jsx
@@ -1,30 +1,37 @@
 var MessageBox = React.createClass({
   getInitialState: function() {
-    return {
-      messages: [
-        { id: 1, user: 'Tom',   text: 'Good morning' },
-        { id: 2, user: 'John',  text: 'Good afternoon' },
-        { id: 3, user: 'Emily', text: 'Good evening' }
-      ]
-    }
+    return { messages: [] };
   },
   handleMessageSubmit: function(message) {
     message.id = new Date();
     var newMessages = this.state.messages.concat(message);
     this.setState({ messages: newMessages });
   },
+  componentDidMount: function() {
+    $.ajax({
+      url:      this.props.url,
+      dataType: 'json',
+      cache:    false,
+      success: function(messages) {
+        this.setState({ messages: messages });
+      }.bind(this),
+      eror: function(_xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
 
   render: function() {
     var messageItems = this.state.messages.map(function(message) {
       return (
           <MessageItem key={message.id} message={message}/>
-        );
+          );
     });
     return (
         <div className="MessageBox">
-          {messageItems}
-          <MessageForm onMessageSubmit={this.handleMessageSubmit}/>
+        {messageItems}
+        <MessageForm onMessageSubmit={this.handleMessageSubmit}/>
         </div>
-      )
+        )
   }
 });
